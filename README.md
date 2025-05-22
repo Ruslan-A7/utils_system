853c74d337343bb5bb8713b9301c450a451c9fe5
RA7 <169072120+Ruslan-A7@users.noreply.github.com>
2025-05-22 16:27:52 +0300

    update

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 ## English
 System utilities and framework constants.
+Functions to convert paths using "\\", "/", greater more sign (>) or dot (.) as directory separators to a valid path according to the directory separator for the current operating system, designed to unify internal paths across framework packages.
+Also contains a constant DS to store the directory separator for the current operating system.
 
 Notes:
 - Semantic versioning is used accordance with https://semver.org.
@@ -15,6 +17,8 @@ Licensed under [RA7 Open Free License](https://ra7.iuid.cc/license)
 
 ## Українська
 Системні утиліти та константи фреймворка.
+Функції для перетворення шляхів із використанням "\\", "/", знака «більше» (>) або «крапки» (.) у якості роздільника директорій, на дійсний шлях згідно роздільника директорій для поточної операційної системи, що призначено для уніфікації внутрішніх шляхів у всіх пакетах фреймворка.
+Також містить константу DS для збереження роздільника директорій для поточної операційної системи.
 
 Примітки:
 - Використовується семантичне версіювання згідно https://semver.org/lang/uk.
---
+++ src/separators/definition.php
@@ -0,0 +1,65 @@
+<?php
+if (!defined('DS')) {
+    /**
+     * DS - скорочений варіант DIRECTORY_SEPARATOR (стандартний роздільник каталогів для поточної операційної системи).
+     *
+     * Використовувати при побудові шляхів до файлу для забезпечення функціонування коду на різних ОС.
+     *
+     * ВАЖЛИВО! Щоб встановити власне значення для цієї константи - потрібно визначити її ДО підключення файлу vendor/autoload.php що містить автозавантажувач від Composer.
+     */
+    define('DS', DIRECTORY_SEPARATOR);
+} elseif (DS !== '/' && DS !== '\\') {
+    exit('Invalid DS! This message came from package "ra7/utils_system" in file "src/separators.php"');
+}
+
+
+
+if (!defined('NESTING_SEPARATOR')) {
+    /**
+     * NESTING_SEPARATOR - роздільник вкладеності, що використовується при побудові шляхів без використання стандартного роздільника каталогів,
+     * а також для масивів та інших структур що мають вкладеність.
+     *
+     * Допускається використовувати знак більше '>' або крапку '.' як в інших фреймворках.
+     *
+     * ВАЖЛИВО! Щоб встановити власне значення для цієї константи - потрібно визначити її ДО підключення файлу vendor/autoload.php що містить автозавантажувач від Composer.
+     */
+    define('NESTING_SEPARATOR', '>');
+} elseif (NESTING_SEPARATOR !== '>' && NESTING_SEPARATOR !== '.') {
+    exit('Invalid NESTING_SEPARATOR! This message came from package "ra7/utils_system" in file "src/separators.php"');
+}
+
+if (!defined('NS')) {
+    /**
+     * NS - скорочений варіант NESTING_SEPARATOR (роздільник вкладеності).
+     * Обов'язково має відповідати NESTING_SEPARATOR!
+     */
+    define('NS', NESTING_SEPARATOR);
+} elseif (NS !== NESTING_SEPARATOR) {
+    exit('Invalid NS! This message came from package "ra7/utils_system" in file "src/separators.php"');
+}
+
+
+
+if (!defined('BACKWARD_NESTING_SEPARATOR')) {
+    /**
+     * BACKWARD_NESTING_SEPARATOR - роздільник вкладеності (для зворотного порядку - тобто для підйому на попередній/батьківський рівень),
+     * що використовується при побудові шляхів без використання стандартного роздільника каталогів, а також для масивів та інших структур що мають вкладеність.
+     *
+     * Допускається використовувати знак більше '<' або стрілочку вверх '^' (карет).
+     *
+     * ВАЖЛИВО! Щоб встановити власне значення для цієї константи - потрібно визначити її ДО підключення файлу vendor/autoload.php що містить автозавантажувач від Composer.
+     */
+    define('BACKWARD_NESTING_SEPARATOR', '<');
+} elseif (BACKWARD_NESTING_SEPARATOR !== '<' && BACKWARD_NESTING_SEPARATOR !== '^') {
+    exit('Invalid BACKWARD_NESTING_SEPARATOR! This message came from package "ra7/utils_system" in file "src/separators.php"');
+}
+
+if (!defined('BNS')) {
+    /**
+     * BNS - скорочений варіант BACKWARD_NESTING_SEPARATOR (роздільник вкладеності для зворотного порядку - тобто для підйому на попередній/батьківський рівень).
+     * Обов'язково має відповідати BACKWARD_NESTING_SEPARATOR!
+     */
+    define('BNS', BACKWARD_NESTING_SEPARATOR);
+} elseif (BNS !== BACKWARD_NESTING_SEPARATOR) {
+    exit('Invalid BNS! This message came from package "ra7/utils_system" in file "src/separators.php"');
+}
